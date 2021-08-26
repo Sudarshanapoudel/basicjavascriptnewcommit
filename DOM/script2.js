28f8db066efa9850e2994e8be0cd7a126be7fbbf
Sudarshanapoudel <sudarpoudel1990@gmail.com>
2021-08-26 09:32:13 -0500

    basicjavascriptnewcommit

---
+++ DOM/script2.js
@@ -0,0 +1,13 @@
+
+let headingOne =document.querySelector('h1')
+headingOne.addEventListener('click', function(){
+    headingOne.style.color = "blue"
+    
+})
+
+//document.write('<p>hello i am  created from javascript <p>')
+//document.write(' <p>hello my name is sudarshana <p>') 
+
+// Javascript can be used to create , retrive , update and delete html element 
+
+//and  update its properties and values based on the user action
