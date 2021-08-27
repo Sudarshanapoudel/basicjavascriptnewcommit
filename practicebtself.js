87026bb686eb3a2b74cbe95fa35c4e2938137a28
Sudarshanapoudel <sudarpoudel1990@gmail.com>
2021-08-26 20:06:50 -0500

    init

---
+++ practicebtself.js
@@ -0,0 +1,36 @@
+
+//1. Write a JavaScript program to display the current day and time in the following format.  Go to the editor
+//Sample Output : Today is : Tuesday.
+//Current time is : 10 PM : 30 : 38 //need to do
+
+// Write a JavaScript program to calculate multiplication and division of two numbers 
+
+// let firstNum = 12
+// let secondNumber=10
+
+
+// function number(firstNum,secondNumber){
+//     console.log(firstNum*secondNumber)
+//     console.log(firstNum/secondNumber)
+// }
+// number(firstNum,secondNumber)
+
+// console.log(".................")
+
+// Write a JavaScript program to get the difference between a given number and 13, if the number is greater than 13 return double the absolute difference
+
+// let givenno1 = 23
+// let ournum = 13
+// function difference(givenno1,ournum){
+//     console.log(givenno1 - ournum)
+// }
+// difference(givenno1,ournum)
+let givenno1 = 13
+ function difference(givenno1){
+   if(givenno1<=13)
+       return 13 - givenno1
+   else 
+       return(givenno1-13) *2   
+ }
+console.log(difference(23))
+console.log(difference(11))
